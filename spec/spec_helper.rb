36d9677b7271ac4899f13c7638f40fa297389bc6
Josh Jordan <josh.jordan@gmail.com>
2014-06-18 15:43:29 -0400

    Properly reference client class in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
-LinkedIn.load('spec/test_app.yml')
+LinkedIn::Client.load('spec/test_app.yml')
 LinkedIn::Client.configure do |config|
   config.access_token = 'AQUJwEF40pJUbVxsW_mujQ3QCiXvlTnMFk55SlfVPYRcAPdsn1oE1Hm8Ldlc61o57k96i04ufG81KFdPJIOSJswXsGyZ0tk9IMZea8sfNXMGMnZgikQJUQPkmRVYVw9BP1qH9tp7hJF32DQtzkBB_NE8xPASPVgXVWbbntChGyYqqDvF1p8'
 end
