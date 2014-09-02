ee7228b2e6188dbef089705c6202ed585fd76b4f
Bob Breznak <bob.breznak@gmail.com>
2014-09-02 07:18:53 -0400

    Fix typo with config vs configuration

--- lib/linkedin/api/authentication.rb
+++ lib/linkedin/api/authentication.rb
@@ -2,7 +2,7 @@ module LinkedIn
   module API
     module Authentication
       def authorize_url(**params)
-        params.reverse_merge! config.to_h.slice :scope, :state, :redirect_uri
+        params.reverse_merge! configuration.to_h.slice :scope, :state, :redirect_uri
         params[:scope] = serialize_scope params[:scope]
         credentials.auth_code.authorize_url params
       end
@@ -10,7 +10,7 @@ module LinkedIn
       def request_access_token(authorization_code, params = {})
         raise Error::CSRF.new state, params[:state] if params[:state] && params[:state] != state
 
-        params.reverse_merge! redirect_uri: config.redirect_uri
+        params.reverse_merge! redirect_uri: configuration.redirect_uri
         opts = { mode: :query, param_name: 'oauth2_access_token' }
 
         self.access_token = auth_code.get_token authorization_code, params, opts
